855cfeaef3a08c24de791af780d6bce18964f28d
tylurp <tylurp1@gmail.com>
2019-11-27 15:05:39 -0800

    remove extra stuff, keep template small

--- R/ui_tabs.R
+++ R/ui_tabs.R
@@ -1,24 +1,17 @@
 tab_dashboard <- function() {
   tabItem(
     tabName = "dashboard",
-    fluidRow(
-      valueBoxOutput("box1"),
-      valueBoxOutput("box2"),
-      valueBoxOutput("box3"),
-      box(
-        width = 12,
-        title = "Plot 1",
-        status = "primary",
-        solidHeader = TRUE,
-        collapsible = TRUE,
-        plotOutput("plot1")
-      )
+    fluidPage(
+      h1("Dashboard!")
     )
   )
 }
 
 tab_widgets <- function() {
   tabItem(
-    tabName = "widgets"
+    tabName = "widgets",
+    fluidPage(
+      h1("Widgets!")
+    )
   )
 }
